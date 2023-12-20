b197908acf5553728b4f69ccf28ebb9f1c6fafba
Tweni47 <derrickayaya@gmail.com>
2023-12-20 12:42:47 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+import './App.css'
+import quotes from './assets/quotes.json'
+import {FaTwitter, FaQuoteLeft, FaQuoteRight} from 'react-icons/fa'
+
+interface Quote{
+  quote: string;
+  author: string;
+}
+
+const getRandomQuote = (): Quote => {
+  return quotes[Math.floor(Math.random() * quotes.length)]
+}
+
+const getRandomColor = (): string =>{
+  const red = Math.floor(Math.random() * 150)
+  const green = Math.floor(Math.random() * 150)
+  const blue = Math.floor(Math.random() * 150)
+
+  return `rgb(${red}, ${green}, ${blue})`
+}
+
+const transition = 'all 2s'
+
+
+function App() {
+  const [quote, setQuote] = useState<Quote>(getRandomQuote())
+
+  const [randomColor, setRandomColor] = useState(getRandomColor())
+
+  const changeQuote = () => {
+    setQuote(getRandomQuote())
+    setRandomColor(getRandomColor())
+  }
+
+  return (
+    <div className='background' style={{backgroundColor: randomColor, transition}}>
+      <div id="quote-box">
+        <div className="quote-content" style={{color: randomColor, transition}}>
+          <h2 id="text">
+          <FaQuoteLeft size={30} style={{marginRight: '10px'}} />
+            {quote.quote}
+          <FaQuoteRight size={30} style={{marginLeft: '10px'}} />
+          </h2>
+          <h4 id="author">- {quote.author}</h4>
+        </div>
+        <div className="buttons">
+          <a 
+            href="twitter.com/intent/tweet" id='tweet-quote'
+            style={{
+              backgroundColor: randomColor,
+              transition,
+              marginRight: 10
+            }}
+            >
+              <FaTwitter color='white'/>
+            </a>
+            <button 
+              id="new-quote" 
+              onClick={changeQuote}
+              style={{backgroundColor: randomColor, transition}}
+            >
+              Change Quote
+            </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
